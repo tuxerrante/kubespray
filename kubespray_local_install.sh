2b4a823fed6c88f206332ab22b429c6b06d8d1bc
aaffinito <aaffinito@expertsystem.com>
2020-12-09 13:03:17 +0100

    Testing new user config. Remove-node error

--- kubespray_local_install.sh
+++ kubespray_local_install.sh
@@ -2,29 +2,27 @@
 # ======================================================
 #   kubespray first startup on a test machine          =
 # ======================================================
-sudo yum install -y ansible-2.9.*
 
 # git clone https://github.com/kubernetes-sigs/kubespray.git
 # cd kubespray || exit 1
-git checkout expert
 
 # Install dependencies from ``requirements.txt``
 # sudo yum install -y ansible-2.9.15 python-jinja2 python-netaddr
 sudo pip3 install -r requirements.txt
 
 # Copy ``inventory/sample`` as ``inventory/expert_cluster``
-cp -rfp inventory/expert inventory/expert_cluster
+# cp -rfp inventory/expert inventory/expert_cluster
 
 # Update Ansible inventory file with inventory builder
 # declare -a IPS=(10.10.1.3 10.10.1.4 10.10.1.5)
 my_ip=$(hostname -i)
 declare -a IPS="($my_ip)"
 
-CONFIG_FILE=inventory/expert_cluster/hosts.yaml python3 contrib/inventory_builder/inventory.py "${IPS[@]}"
+CONFIG_FILE=inventory/expert/hosts.yaml python3 contrib/inventory_builder/inventory.py "${IPS[@]}"
 
-# Review and change parameters under ``inventory/expert_cluster/group_vars``
-# cat inventory/expert_cluster/group_vars/all/all.yml
-# cat inventory/expert_cluster/group_vars/k8s-cluster/k8s-cluster.yml
+# Review and change parameters under ``inventory/expert/group_vars``
+# cat inventory/expert/group_vars/all/all.yml
+# cat inventory/expert/group_vars/k8s-cluster/k8s-cluster.yml
 
 # Enable containerd runtime
 if ! sudo systemctl status containerd 1>/dev/null; then 
@@ -39,7 +37,7 @@ fi
 # The option `--become` is required, as for example writing SSL keys in /etc/,
 # installing packages and interacting with various systemd daemons.
 # Without --become the playbook will fail to run!
-ansible-playbook -i inventory/expert_cluster/hosts.yaml --become --become-user=root cluster.yml
+ansible-playbook -i inventory/expert/hosts.yaml --become --become-user=root cluster.yml
 
 mkdir -p "$HOME/.kube"
 sudo cp -f -i /etc/kubernetes/admin.conf "$HOME/.kube/config"
